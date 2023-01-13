46992f5085fe5aefbba456058f9539dc34b81518
amayurBAH <122626714+amayurBAH@users.noreply.github.com>
2023-01-13 11:40:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function showMessage() {
-  document.getElementByID("hidden1").style.display = 'block';
+function revealMessage() {
+	document.getElementById("hiddenMessage").style.display = 'block';
 
+}
+
+function revealMessage1() {
+	document.getElementById("hidden1").style.display = 'block';
+	
 }
