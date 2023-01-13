09a35db521b19c78d428f5354c05b10bc55fca34
amayurBAH <122626714+amayurBAH@users.noreply.github.com>
2023-01-13 12:07:35 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function revealMessage() {
-	document.getElementById("hiddenMessage").style.display = 'block';
-
-}
-
-function revealMessage1() {
-	document.getElementById("hidden1").style.display = 'block';
-	
-}
