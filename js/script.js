e5abcdbed977742a062ed1ac20c720c2c531f65e
amayurBAH <122626714+amayurBAH@users.noreply.github.com>
2023-01-19 22:15:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-function revealMessage() {
-	document.getElementById("hiddenMessage").style.display = 'block';
+function user_api() {
+	let input_param = document.getElementById("UserID").value
+	let input_url = ("https://aaexprc570.execute-api.us-east-1.amazonaws.com/getsecrets?dbuser="+input_param)
+	alert(input_url)
+	fetch(input_url,{headers:{'Content-Type':'application/json'},mode:'cors'})
+	.then(res => res.json())
+	.then(posts => console.log(posts))
+ 
 
-}
-
-function revealMessage1() {
-	document.getElementById("hidden1").style.display = 'block';
-	
 }
