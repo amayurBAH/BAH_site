e0aed336027bcd111ad543d2470ed9d125de2fa5
amayurBAH <122626714+amayurBAH@users.noreply.github.com>
2023-01-19 22:21:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+// function revealMessage() {
+// 	document.getElementById("hiddenMessage").style.display = 'block';
+
+// }
+
+// function revealMessage1() {
+// 	document.getElementById("hidden1").style.display = 'block';
+	
+// }
+
+// // const input = prompt("What is your username?")
+// // alert(`Your name is ${input}`);
+
+
+
+
+function returnSecret() {
+
+	let input = document.getElementById("UserID").value
+	alert("https://aaexprc570.execute-api.us-east-1.amazonaws.com/getsecrets?dbuser="+input)
+	let endpointURL = ("https://aaexprc570.execute-api.us-east-1.amazonaws.com/getsecrets?dbuser="+input)
+
+}
+
+
+
+
+
+
+
+
 function user_api() {
 	let input_param = document.getElementById("UserID").value
 	let input_url = ("https://aaexprc570.execute-api.us-east-1.amazonaws.com/getsecrets?dbuser="+input_param)
@@ -8,3 +39,72 @@ function user_api() {
  
 
 }
+
+// class UserApiClass {
+// 	constructor(http) {
+// 		this.http = http;
+// 	}
+// }
+
+
+// function user_api() {
+// 	let input_param = document.getElementById("UserID").value
+// 	let input_url = ("https://aaexprc570.execute-api.us-east-1.amazonaws.com/getsecrets?dbuser="+input_param)
+
+// 	const api_call = http.get(input_url, function(api_res){
+// 		let body_chunks = [];
+// 		api_res.on('data', function(chunk) {
+// 			body_chunks.push(chunks);
+// 		});
+// 		api_res.on('end', function() {
+// 			let body = Buffer.concat(body_chunks);
+// 			body = JSON.parse(body);
+
+// 			if('error' in body)
+// 				console.log(body.error)
+// 			else
+// 				console.log(body);
+// 		});
+// 	}); 
+
+// 	api_call.on('error', function(e) {
+// 		console.log(e.message);
+// 	})
+
+// 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let val = user_api()
+// let api_url = ("https://aaexprc570.execute-api.us-east-1.amazonaws.com/getsecrets?dbuser="+val)
+// alert(api_url)
+	
+// async function getapi(api_url){
+
+// 	const response = await fetch(api_url);
+// 	var data = await response.json();
+// 	console.log(data);
+// 	if (response) {
+// 		hideloader();
+// 	} show(data);
+
+// }
+
+// getapi(api_url)
+
+// function hideloader() {
+// 	document.getElementById('loading').style.display = 'none';
+// }
+
+
